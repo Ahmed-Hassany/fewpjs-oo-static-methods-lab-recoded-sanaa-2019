e9fcf427e9fc4f06f23cb22cb79a1b3cfe83a8a0
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-12-08 09:16:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,16 +7,29 @@ class Formatter {
   static sanitize(s){
     return s.replace(/[^A-Za-z0-9 '-]/g, '')
   }
+  class Formatter {
+  //add static methods here
+  
+  static capitalize(s){
+    return s.charAt(0).toUpperCase()+s.slice(1)
+  }
+  static sanitize(s){
+console.log("hi")
+    return s.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
   
   static titleize(s){
     let ex=["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
     let words = s.split(" ")
-    words.forEach((word,index)=>{
+    let newS=words.map((word,index)=>{
       if(index==0)
-      word = Formatter.capitalize(word)
+      return this.capitalize(word)
       else if(!ex.includes(word))
-      word = Formatter.capitalize(word)
+      return this.capitalize(word)
+else
+return word
     })
-    return words.join( " " );
+    return newS.join( " " );
   }
+}
 }
