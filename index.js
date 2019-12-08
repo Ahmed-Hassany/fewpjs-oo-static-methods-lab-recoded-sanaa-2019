44620e0c72e728d5d532a4b5b02a8353d0fc8740
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-12-08 09:01:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Formatter {
     return s.charAt(0).toUpperCase()+s.slice(1)
   }
   static sanitize(s){
-    return s.replace(/[^0-9a-zA-Z -']/gi, '')
+    return s.replace(/[^0-9a-zA-Z -'!]/gi, '')
   }
   
   static titleize(s){
